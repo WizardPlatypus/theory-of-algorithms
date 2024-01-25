7d7a2a2366598879245862ef6543681bd56f8ede
WizardPlatypus <f.volik.m@gmail.com>
2024-01-25 11:10:42 +0200

    Prototyping varios rule representation ideas
    Signed-off-by: WizardPlatypus <f.volik.m@gmail.com>

--- 00-24_Turing-machine/main.cpp
+++ 00-24_Turing-machine/main.cpp
@@ -1,5 +1,7 @@
 #include <cstddef>
 #include <iostream>
+#include <istream>
+#include <map>
 #include <stdint.h>
 #include <vector>
 
@@ -56,7 +58,76 @@ Node *init_nodes(const std::vector<uint64_t> &list) {
   return first;
 }
 
+struct Rule {
+  char state_in;
+  char symbol_in;
+  char state_out;
+  char symbol_out;
+  // move > 0 => right
+  // move < 0 => left
+  // move == 0 => stay
+  int8_t move;
+};
+
+std::istream &operator>>(std::istream &in, Rule &rule) {
+  in >> rule.state_in >> rule.symbol_in >> rule.state_out >> rule.symbol_out >>
+      rule.move;
+  return in;
+}
+
+struct RuleKey {
+  char state;
+  char symbol;
+
+  bool operator==(const RuleKey &other) const {
+    return this->state == other.state && this->symbol == other.symbol;
+  }
+
+  bool operator<(const RuleKey &other) const {
+    if (this->state == other.state) {
+      return this->symbol < other.symbol;
+    }
+    return this->state < other.state;
+  }
+};
+
+std::istream &operator>>(std::istream &in, RuleKey &key) {
+  in >> key.state >> key.symbol;
+  return in;
+}
+
+enum Move {
+  Left,
+  Right,
+  None
+};
+  
+std::istream & operator>>(std::istream & in, Move & move) {
+  char c;
+  in >> c;
+  if (c == 'L') {
+    move = Move::Left;
+  } else if (c == 'R') {
+    move = Move::Right;
+  } else {
+    move = Move::None;
+  }
+  return in;
+}
+
+struct RuleValue {
+  char state;
+  char symbol;
+  Move move;
+};
+
+std::istream &operator>>(std::istream &in, RuleValue &value) {
+  in >> value.state >> value.symbol >> value.move;
+  return in;
+}
+
 int main() {
+  /*
   std::vector<uint64_t> nums{3, 4};
   Node *first = init_nodes(nums);
   Node *curr = first;
@@ -66,4 +137,23 @@ int main() {
   } while (curr != nullptr);
   std::cout << std::endl;
   return 0;
+  */
+  std::map<RuleKey, RuleValue> rules;
+  for (int i = 0; i < 3; i++) {
+    RuleKey key;
+    RuleValue value;
+    std::cin >> key >> value;
+    rules[key] = value;
+  }
+
+  for (const auto &rule : rules) {
+    std::cout << "q" << rule.first.state << rule.first.symbol << " -> q"
+              << rule.second.state << rule.second.symbol;
+    if (rule.second.move > 0) {
+      std::cout << "R";
+    } else if (rule.second.move < 0) {
+      std::cout << "L";
+    }
+    std::cout << std::endl;
+  }
 }
